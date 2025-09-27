ebfb94d69ab84975eb8dc298421b94c13b2410c8
saken555 <exprechi.p@gmail.com>
2025-09-27 07:55:20 +0500

    feat: Implement Contacts index page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -11,7 +12,8 @@ class ContactController extends Controller
      */
     public function index()
     {
-        //
+        $contacts = Contact::with('client')->get();
+        return view('contacts.index', ['contacts' => $contacts]);
     }
 
     /**
